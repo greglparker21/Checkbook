0ed8af7d1a0d485d9312a2fd0df897374bce435c
Greg Parker <greglparker21@gmail.com>
2015-02-23 16:38:53 -0600

    debugging
    got the new transactions parsed.   now to update the main spreadsheet
    register.

--- UpdateTransactions.py
+++ UpdateTransactions.py
@@ -4,28 +4,43 @@
     02/16/2015
 '''
 # import Namespaces
+import sys
 import xlrd
-import csv
+import string
 
 class newTransactions():
     def __init__(self,name):
       self.name = name
 
-    def convertToTrueCSV(self,o_location,n_location):
+    def convertToMacCSV(self,o_location,n_location):
+
       fi = open(o_location, 'rb')
       data = fi.read()
       fi.close()
       fo = open(n_location, 'wb')
-      fo.write(data.replace('\x00', ''))
-      #fo.write(data.replace('\xff',''))
+      readable = data.rstrip("\n").decode("utf-16")
+      fo.write(readable)
       fo.close()
 
     def getNewTransactions(self,location):
-        with open(location,'rb') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                #print (row.keys())
-                print (row['Amount'])
+        fi = open(location,'rb')
+        i=0
+        values_dict = {}
+        for row in fi:
+           i+=1
+           row = string.replace(row,' ','')
+           row = string.replace(row,'"','')
+           row = string.replace(row,"'",'"')
+           row = string.replace(row,'-','')
+           row = string.replace(row,'\r\n','')
+           if i>1:
+              values=string.split(row,',')
+              values_dict.setdefault('Payee', []).append(values[0])
+              values_dict.setdefault('Date', []).append(values[1])
+              values_dict.setdefault('Amount', []).append(float(values[2]))
+        self.transactions = values_dict
+        fi.close()
 
-    #def parseTransactions(self):
-    #def updateMainSpreadSheet(self):
+    def updateMainSpreadSheet(self):
+
+
--- main.py
+++ main.py
@@ -5,15 +5,18 @@
 import sys
 from UpdateTransactions import newTransactions
 import os
+import string
+
 
 def main(argv=None):
    cwd = os.getcwd()
+   # will eventually pass this in from the command line
    oldtranslocation = cwd + '/Data/transactions_Checking_02-14-2015.csv'
    newtranslocation = cwd + '/Data/Workbook1.csv'
-   nt = newTransactions('MyCheckBookTransactions')
-   #nt.convertToTrueCSV(oldtranslocation,newtranslocation)
-   nt.getNewTransactions(newtranslocation)
 
+   nt = newTransactions('MyCheckBookTransactions')
+   #nt.convertToMacCSV(oldtranslocation,newtranslocation)
+   nt.getNewTransactions(newtranslocation)
 
 if __name__ == "__main__":
     sys.exit(main())
